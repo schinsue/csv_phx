a401385a0ce3e1646c0143632be9e1128fadb747
Sergio Chin-Sue <s.chinsue@gmail.com>
2018-04-24 23:58:25 +0100

    [Feature] Seed file is now importing from csv files

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,11 +11,15 @@
 # and so on) as they will fail if something goes wrong.
 
 defmodule DBSeed do
+  alias CsvPhx.Reports
 
   def import(path) do
     path
     |> get_folder_list()
     |> Enum.flat_map(&csv_to_enum_list/1)
+    |> Enum.map(&convert_to_schema/1)
+    |> Task.async_stream(&Reports.create_crime/1, [max_concurrency: 10_000])
+    |> Enum.map(fn {:ok, x} -> x end)
   end
 
   def get_folder_list(path) do
@@ -29,6 +33,28 @@ defmodule DBSeed do
     |> Enum.to_list()
   end
 
+  def convert_to_schema(crime) do
+    %{
+      context: crime["Context"],
+      crime_id: crime["Crime ID"],
+      crime_type: crime["Crime type"],
+      falls_within: crime["Falls within"],
+      last_outcome_category: crime["Last outcome category"],
+      latitude: crime["Latitude"] |> parse_float(),
+      location: crime["Location"],
+      longitude: crime["Longitude"] |> parse_float(),
+      lsoa_code: crime["LSOA code"],
+      lsoa_name: crime["LSOA name"],
+      month: crime["Month"],
+      reported_by: crime["Reported by"],
+    }
+  end
+
+  def parse_float(""), do: 0
+  def parse_float(string) do
+    {float, _} = string |> Float.parse()
+    float
+  end
 end
 
 DBSeed.import("./data/")
