324071d397a2d1f2933257c8ee0329e73736ce06
Sergio Chin-Sue <s.chinsue@gmail.com>
2018-04-24 23:59:04 +0100

    [Cleanup] Limit validation to most important fields

--- lib/csv_phx/reports/crime.ex
+++ lib/csv_phx/reports/crime.ex
@@ -24,6 +24,6 @@ defmodule CsvPhx.Reports.Crime do
   def changeset(crime, attrs) do
     crime
     |> cast(attrs, [:crime_id, :month, :reported_by, :falls_within, :longitude, :latitude, :location, :lsoa_code, :lsoa_name, :crime_type, :last_outcome_category, :context])
-    |> validate_required([:crime_id, :month, :reported_by, :falls_within, :longitude, :latitude, :location, :lsoa_code, :lsoa_name, :crime_type, :last_outcome_category, :context])
+    |> validate_required([:crime_id, :month, :reported_by])
   end
 end
